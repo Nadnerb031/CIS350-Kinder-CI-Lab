18a5d0c8ca2f488badfa38977bde1eb46a8b2b9a
Brendan J. Kinder <kinderbr@eos05.cis.gvsu.edu>
2022-11-28 13:51:28 -0500

    power

--- math_functions.py
+++ math_functions.py
@@ -10,10 +10,18 @@ def multiply_numbers(a, b):
 def divide_numbers(a, b):
 	return a / b	
 
+def power_numbers(a, b):
+	c = a
+	while b > 1:
+		a = a * c
+		b -= 1
+	return a
+
 if __name__ == "__main__":
 	print("Adding:", add_numbers(2,4))
 	print("Subtracting:", subtract_numbers(9,2))
 	print("Multiplying:", multiply_numbers(2,3))
 	print("Dividing:", divide_numbers(6,3))
+	print("Power:", power_numbers(2,3))
 	
 
